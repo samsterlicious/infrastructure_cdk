91f3e20e8d47e2052ace6b7ae1491071ee3be3c9
samsterlicious <pgreene864@gmail.com>
2021-03-24 21:20:13 -0400

    removed stack from infrastructure bin

--- bin/infrastructure_cdk.ts
+++ bin/infrastructure_cdk.ts
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { InfrastructureCdkStack } from '../lib/infrastructure_cdk-stack';
-import { CreateSecretsStack } from '../lib/create_secrets-stack';
+import { InfrastructureCdkStack } from '../lib/infrastructure_cdk-stack'; 
 
 const app = new cdk.App();
 new InfrastructureCdkStack(app, 'InfrastructureCdkStack');
-new CreateSecretsStack(app, 'CreateSecretsStack');
